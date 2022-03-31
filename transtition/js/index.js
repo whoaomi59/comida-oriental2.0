f86430acc1420183ece96d3c22aa5be2e2b0b627
whoaomi59 <whoaomi11@gmail.com>
2022-03-31 14:26:46 -0500

    g

--- transtition/js/index.js
+++ transtition/js/index.js
@@ -3,18 +3,40 @@ function buscar(){
     navlupa.addEventListener("click", mifuncion);
 
     function mifuncion(){
-        document.getElementById("header").innerHTML = "";
+        document.getElementById("boxheader").innerHTML = "";
         llamarventana();
     }
     function llamarventana(){
-        let contenedor = document.getElementById("header");
+        let contenedor = document.getElementById("boxRemplazo");
         let div =  document.createElement("div");
-        let iconolupa= document.createElement("input");
-        
+        let label = document.createElement("label")
+        let input= document.createElement("input");
+        input.placeholder = "¿Qué estás buscando?";
+        let label2 = document.createElement("label")
+/*===============hijos y relaciones=============*/
         contenedor.appendChild(div);
-        div.setAttribute("class","box")
-        vid.appendChild(iconolupa);
-        iconolupa.setAttribute("class","inputSear")
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(label2);
+        /*===========clases=========*/
+        div.setAttribute("class","boxRemplazo");
+        label.setAttribute("class","fa fa-search icob");
+        input.setAttribute("class","inputSear");
+        label2.setAttribute("class","fa fa-close close");
+        label2.setAttribute("id","closes");
+
+        
     }
+   
     
 }
+
+/*function cerrar(close){
+    document.getElementById("boxRemplazo").innerHTML = "";
+    llamarventana();
+    alert.apply("hello")
+    function close(){
+        let cerarVentana = document.getElementById("boxheader");
+    }*/
+
+
